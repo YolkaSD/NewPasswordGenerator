199c02dcaa9bc1ae98de1578e74ba892597c46dd
YolkaSD <yolka.sd@gmail.com>
2022-12-14 23:28:50 +0700

    Added class field SymbolGeneratorImpl.class

--- src/main/implementations/SymbolGeneratorImpl.java
+++ src/main/implementations/SymbolGeneratorImpl.java
@@ -3,10 +3,16 @@ package main.implementations;
 import main.interfaces.SymbolGenerator;
 
 public class SymbolGeneratorImpl implements SymbolGenerator {
+    private static final String CHAR_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_UPPERCASE = CHAR_LOWERCASE.toUpperCase();
+    private static final String ALL_CHARS = CHAR_UPPERCASE + CHAR_LOWERCASE;
+    private static final String NUMBERS = "0123456789";
+    private static final String CHAR_SPECIAL = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 
     @Override
     public char generateRandomLetter() {
+
         return 0;
     }
 
