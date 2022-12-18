d86e7ecec382d1e3fee864ca028b4f777506e56a
Slava Rybkin <yolka.sd@gmail.com>
2022-12-18 14:40:18 +0700

    Created private method getRandomChar

--- src/main/implementations/SymbolGeneratorImpl.java
+++ src/main/implementations/SymbolGeneratorImpl.java
@@ -2,22 +2,14 @@ package main.implementations;
 import main.interfaces.SymbolGenerator;
 
 public class SymbolGeneratorImpl implements SymbolGenerator {
-    private static final String CHAR_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static final String CHAR_UPPERCASE = CHAR_LOWERCASE.toUpperCase();
-    private static final String ALL_CHARS = CHAR_UPPERCASE + CHAR_LOWERCASE;
+    private static final String CHAR_ALL_CASE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     //NUMBERS is that Rudiment? See implementation generateRandomNumber()
     private static final String NUMBERS = "0123456789";
     private static final String CHAR_SPECIAL = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-    private int length, index;
-
-
-   /* Also note that methods generateRandomLetter and generateRandomSymbol have the same implementation */
 
     @Override
     public char generateRandomLetter() {
-        length = ALL_CHARS.length();
-        index = Character.forDigit((int) (Math.random() * length), 10);
-        return ALL_CHARS.charAt(index);
+        return getRandomChar(CHAR_ALL_CASE);
     }
 
     @Override
@@ -27,8 +19,10 @@ public class SymbolGeneratorImpl implements SymbolGenerator {
 
     @Override
     public char generateRandomSymbol() {
-        length = CHAR_SPECIAL.length();
-        index = Character.forDigit((int) (Math.random() * length), 10);
-        return ALL_CHARS.charAt(index);
+        return getRandomChar(CHAR_SPECIAL);
+    }
+
+    private char getRandomChar(String original){
+        return original.charAt((int) (Math.random() * original.length()));
     }
 }
