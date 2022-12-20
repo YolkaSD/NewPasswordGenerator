c0cf5412a038be4d621b402dfe14051567d05866
Slava Rybkin <yolka.sd@gmail.com>
2022-12-20 17:49:37 +0700

    Modern generatePassword method

--- src/main/implementations/PasswordGeneratorImpl.java
+++ src/main/implementations/PasswordGeneratorImpl.java
@@ -4,8 +4,8 @@ import main.interfaces.PasswordGenerator;
 import main.interfaces.SymbolGenerator;
 
 public class PasswordGeneratorImpl implements PasswordGenerator {
-    final static int MIN_LENGTH = 4;
-    final static int MAX_LENGTH = 99;
+    private final static int MIN_LENGTH = 4;
+    private final static int MAX_LENGTH = 99;
     SymbolGenerator symbolGenerator;
     public PasswordGeneratorImpl(SymbolGenerator symbolGenerator){
         this.symbolGenerator = symbolGenerator;
@@ -15,24 +15,13 @@ public class PasswordGeneratorImpl implements PasswordGenerator {
         if(length < MIN_LENGTH || length > MAX_LENGTH) {
             throw new RuntimeException("Incorrect password length");
         }
-        int count = 0;
         StringBuilder passBuilder = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            switch (count) {
-               case 0 -> {
-                   passBuilder.append(symbolGenerator.generateRandomSymbol());
-                   count++;
-               }
-               case 1 -> {
-                   passBuilder.append(symbolGenerator.generateRandomNumber());
-                   count++;
-               }
-               case 2 -> {
-                   passBuilder.append(symbolGenerator.generateRandomLetter());
-                   count -= count;
-               }
-            }
+            passBuilder.append(symbolGenerator.generateRandomSymbol())
+                    .append(symbolGenerator.generateRandomLetter())
+                    .append(symbolGenerator.generateRandomNumber());
         }
-        return String.valueOf(passBuilder);
+        passBuilder.delete(length, length * 3);
+        return passBuilder.toString();
     }
 }
