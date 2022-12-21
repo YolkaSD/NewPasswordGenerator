863451323d0fa64f336c7099b149c941a1eafe67
Slava Rybkin <yolka.sd@gmail.com>
2022-12-21 15:06:41 +0700

    Created CUI v.1

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -1,4 +1,34 @@
 package CUI;
 
+import java.util.Scanner;
+
 public class ConsoleUserInterface {
+    public int[] start(){
+        int[] userArguments = new int[3];
+        String[] arguments = {"numbers", "symbols", "special symbols"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter password options");
+        int index = 0;
+        while (index < 3) {
+            System.out.format("Will the password contain %s?:\n1 - Yes\n0 - No\n2 - Exit\nInput: ", arguments[index]);
+            int input;
+            if (scanner.hasNextInt()) {
+                input = scanner.nextInt();
+                if (input == 0 || input == 1) {
+                    userArguments[index] = input;
+                } else if (input == 2) {
+                    System.exit(-1);
+                } else {
+                    System.out.print("Incorrect input :( ");
+                    index--;
+                }
+            } else {
+                System.out.println("Please restart program and enter the correct number");
+                System.exit(-2);
+            }
+            index++;
+        }
+        return userArguments;
+    }
 }
+
