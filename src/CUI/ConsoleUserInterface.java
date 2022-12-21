d8ef439244abd682005ec2d4b86e48a7d650704f
Slava Rybkin <yolka.sd@gmail.com>
2022-12-22 00:13:25 +0700

    CUI v2.0

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -3,27 +3,27 @@ package CUI;
 import java.util.Scanner;
 
 public class ConsoleUserInterface {
-    public char[] start(){
-        char[] userArguments = new char[3];
-        //String[] arguments Make it a class field?
-        String[] arguments = {"numbers", "symbols", "special symbols"};
+    public StringBuilder start(){
+        StringBuilder userArguments = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter password options");
-        int index = 0;
-        while (index < 3) {
-            System.out.format("Will the password contain %s?:\n1 - Yes\n0 - No\n2 - Exit\nInput: ", arguments[index]);
-                String input = scanner.nextLine();
-                if (input.equals("0") || input.equals("1")) {
-                    userArguments[index] = Character.forDigit(Integer.parseInt(input), 10);
-                } else if (input.equals("2")) {
-                    System.exit(-1);
-                } else {
-                    System.out.print("Incorrect input :( ");
-                    index--;
-                }
-            index++;
+        String configMsg = "1 - number\n" +
+                "2 - symbol\n" +
+                "3 - special symbol\n" +
+                "input: ";
+
+        System.out.format("Plz, enter the required password components in the format:\n%s", configMsg);
+        boolean isCorrectInput = false;
+        while (!isCorrectInput)
+        {
+            String input = scanner.nextLine();
+            //Repeating digits pass "^[1-3]{1,3}$"
+            if (!input.isEmpty() && input.matches("^[1-3]{1,3}$")) {
+                userArguments.append(input);
+                isCorrectInput = true;
+            } else {
+                System.out.format("Plz, Please enter a components as required \n%s", configMsg);
+            }
         }
-        scanner.close();
         return userArguments;
     }
 }
