50db764f374cf1106045bc5fb19fce04ef415e38
Slava Rybkin <yolka.sd@gmail.com>
2022-12-21 16:40:56 +0700

    Fixed method return from int[] to char[]

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -3,8 +3,8 @@ package CUI;
 import java.util.Scanner;
 
 public class ConsoleUserInterface {
-    public int[] start(){
-        int[] userArguments = new int[3];
+    public char[] start(){
+        char[] userArguments = new char[3];
         //String[] arguments Make it a class field?
         String[] arguments = {"numbers", "symbols", "special symbols"};
         Scanner scanner = new Scanner(System.in);
@@ -13,10 +13,10 @@ public class ConsoleUserInterface {
         while (index < 3) {
             System.out.format("Will the password contain %s?:\n1 - Yes\n0 - No\n2 - Exit\nInput: ", arguments[index]);
             int input;
-            if (scanner.hasNextInt()) {
+            if (scanner.hasNextShort()) {
                 input = scanner.nextInt();
                 if (input == 0 || input == 1) {
-                    userArguments[index] = input;
+                    userArguments[index] = Character.forDigit(input, 10);
                 } else if (input == 2) {
                     System.exit(-1);
                 } else {
