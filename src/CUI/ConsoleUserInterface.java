891bcfd7734242ecf7b9e7a1217e38f8ef784e5c
Slava Rybkin <yolka.sd@gmail.com>
2022-12-21 16:49:23 +0700

    CUI v1.1

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -12,21 +12,15 @@ public class ConsoleUserInterface {
         int index = 0;
         while (index < 3) {
             System.out.format("Will the password contain %s?:\n1 - Yes\n0 - No\n2 - Exit\nInput: ", arguments[index]);
-            int input;
-            if (scanner.hasNextShort()) {
-                input = scanner.nextInt();
-                if (input == 0 || input == 1) {
-                    userArguments[index] = Character.forDigit(input, 10);
-                } else if (input == 2) {
+                String input = scanner.nextLine();
+                if (input.equals("0") || input.equals("1")) {
+                    userArguments[index] = Character.forDigit(Integer.parseInt(input), 10);
+                } else if (input.equals("2")) {
                     System.exit(-1);
                 } else {
                     System.out.print("Incorrect input :( ");
                     index--;
                 }
-            } else {
-                System.out.println("Please restart program and enter the correct number");
-                System.exit(-2);
-            }
             index++;
         }
         scanner.close();
