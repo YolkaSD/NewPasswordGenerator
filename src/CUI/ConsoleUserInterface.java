bc48f3f8584eea2859201c66bbba96a81d00452c
Slava Rybkin <yolka.sd@gmail.com>
2022-12-23 16:33:48 +0700

    Improved ConsoleUserInterface added setUserSpecialChar

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ConsoleUserInterface {
     private final static int MIN_LENGTH = 4;
-    private final static int MAX_LENGTH = 99;
     private String userConfig;
     private int userLength;
     private final Scanner scanner = new Scanner(System.in);
@@ -28,42 +27,36 @@ public class ConsoleUserInterface {
                 "5 - several special symbol from the !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
                 "0 - EXIT\n" +
                 "input: ";
-        String configSpecMsg =
-                "enter several special symbol from the !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
-                "0 - EXIT\n" +
-                "input: ";
-
         System.out.format("Plz, enter the required password components in the format:\n%s", configMsg);
-        String firstInput = null;
-        String specInput = null;
+        String firstInput;
         boolean isCorrectInput = false;
         while (!isCorrectInput)
         {
             firstInput = scanner.nextLine();
             if (firstInput.matches("(?!.*(\\d).*\\1)^[1234]{1,4}$")) {
+                userConfig = firstInput + " " + "null";
                 isCorrectInput = true;
             } else if (firstInput.matches("(?!.*(\\d).*\\1)^[1235]{1,4}$")) {
-                System.out.println("Selecting special characters in development");
+                userConfig = firstInput;
+                setUserSpecialChar();
                 isCorrectInput = true;
-                specInput = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n";
             } else if (firstInput.equals("0")){
                 System.exit(-1);
             } else {
                 System.out.format("Plz, Please enter a components as required \n%s", configMsg);
             }
         }
-        userConfig = firstInput + specInput;
     }
     private void setUserLength(){
         String configMsg =
                 "Please enter password length from 4 to 99\n" +
                 "0 - EXIT\n" +
                 "Input: ";
-        System.out.println(configMsg);
+        System.out.print(configMsg);
         boolean isInputCorrect = false;
         while (!isInputCorrect){
             String input = scanner.nextLine();
-            if (input.matches("^\\d{1,2}$") && Integer.parseInt(input) > 3) {
+            if (input.matches("^\\d{1,2}$") && Integer.parseInt(input) >= MIN_LENGTH) {
                 userLength = Integer.parseInt(input);
                 isInputCorrect = true;
             } else if (input.equals("0")) {
@@ -73,6 +66,25 @@ public class ConsoleUserInterface {
             }
         }
 
+    }
+    private void setUserSpecialChar(){
+        String configSpecMsg =
+                "enter several special symbol from the !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
+                "0 - EXIT\n" +
+                "input: ";
+        System.out.print(configSpecMsg);
+        boolean isInputCorrect = false;
+        while (!isInputCorrect){
+            String input = scanner.nextLine();
+            if (!input.isEmpty() && input.matches("^[!\"#$%&'()*+,-.\\/:;<=>?@\\[\\\\\\]^_`{|}~]*$")) {
+                userConfig = userConfig + " " + input;
+                isInputCorrect = true;
+            }else if(input.equals("0")) {
+                System.exit(-1);
+            } else {
+                System.out.println("Plz, Please enter !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+            }
+        }
     }
 }
 
