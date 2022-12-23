eead4a8bb64337c86a22b38b63bc0f87c7009a4c
Slava Rybkin <yolka.sd@gmail.com>
2022-12-23 14:15:38 +0700

    Improved ConsoleUserInterface

--- src/CUI/ConsoleUserInterface.java
+++ src/CUI/ConsoleUserInterface.java
@@ -3,27 +3,42 @@ package CUI;
 import java.util.Scanner;
 
 public class ConsoleUserInterface {
-    public StringBuilder start(){
-        StringBuilder userArguments = new StringBuilder();
+    public String start(){
         Scanner scanner = new Scanner(System.in);
-        String configMsg = "1 - number\n" +
-                "2 - symbol\n" +
-                "3 - special symbol\n" +
+        String configMsg =
+                "1 - digits (0-9)\n" +
+                "2 - upper case (A-Z)\n" +
+                "3 - lower case (a-z)\n" +
+                "4 - special symbol !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
+                "5 - several special symbol from the !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
+                "0 - EXIT\n" +
+                "input: ";
+        String configSpecMsg =
+                "enter several special symbol from the !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n" +
+                "0 - EXIT\n" +
                 "input: ";
 
         System.out.format("Plz, enter the required password components in the format:\n%s", configMsg);
+        String firstInput = null;
+        String specInput = null;
         boolean isCorrectInput = false;
         while (!isCorrectInput)
         {
-            String input = scanner.nextLine();
-            if (!input.isEmpty() && input.matches("(?!.*(\\d).*\\1)^[123]{1,3}$")) {
-                userArguments.append(input);
+            firstInput = scanner.nextLine();
+            if (firstInput.matches("(?!.*(\\d).*\\1)^[1234]{1,4}$")) {
+                isCorrectInput = true;
+            } else if (firstInput.matches("(?!.*(\\d).*\\1)^[1235]{1,4}$")) {
+                System.out.println("Selecting special characters in development");
+                isCorrectInput = true;
+                specInput = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n";
+            } else if (firstInput.equals("0")){
                 isCorrectInput = true;
             } else {
                 System.out.format("Plz, Please enter a components as required \n%s", configMsg);
             }
         }
-        return userArguments;
+        return firstInput + ":" + specInput;
     }
+
 }
 
